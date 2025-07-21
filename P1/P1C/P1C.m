bd91ebbb662b8e96bd6e5901c0addb952067b267
leathermajesty <82501291+leathermajesty@users.noreply.github.com>
2025-07-21 14:51:23 +0530

    practical 1

---
+++ P1/P1C/P1C.m
@@ -0,0 +1,22 @@
+%%Practicatal 1 C - Write a MATLAB program
+%Convert  RGB to Gray scale
+
+
+clc;
+clear;
+close all;
+
+rgb=imread("badminton.jpg");
+gray=rgb2gray(rgb);
+
+figure;
+subplot(2,1,1);
+imshow(rgb);
+title("Originnal rgb")
+
+
+subplot(2,1,2);
+imshow(gray);
+title("Converted to gray")
+
+%imshow(gray)
